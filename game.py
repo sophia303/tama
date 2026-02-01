3899b436e6aa9ff029ac20995a64be87366776a1
Sophia H. <sophie303h@gmail.com>
2026-02-01 15:38:45 -0800

    from buffer pls work

--- game.py
+++ game.py
@@ -9,17 +9,46 @@ import os
 import random
 from sys import exit
 
-display = PyGameDisplay(width=128, height=128)
+gameWidth = 128
+gameHeight = 128
+score = 0
+gameState = "start"
+player = pygame.Rect(40,25,0,10)
+isPlaying = True
+
+display = PyGameDisplay(width=gameWidth, height=gameHeight)
+pygame.display.set_caption("game")
 
 splash = displayio.Group()
 display.root_group = splash
-text = "hackapet stuff"
+text = "score:" + str(score)
 text_area = label.Label(terminalio.FONT, text=text, color=0xFFFFFF, x=10, y=64)
 splash.append(text_area)
 
-while True:
+def startGame():
+    playButton = pygame.Rect(20, 30, 20, 10)
+
+def draw():
+    if gameState == "start":
+        startGame()
+
+def loadImages():
+    images = {}
+    for fileName in os.listdir("assets/"):
+        if fileName.endswith('.png'):
+            path = os.path.join("assets/", fileName)
+            image = pygame.image.load(path).convert_alpha()
+            key = os.path.splitext(fileName)[0]
+            images[key] = image
+    return images
+
+image = loadImages()
+
+while isPlaying:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+    draw()
+    display.refresh()
     time.sleep(0.1)
